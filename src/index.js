b1748a60d166eb1a8e4970935b1738e3ae04b26c
Nikola Kabátová <niki.kabatova@gmail.com>
2022-04-30 12:10:21 +0200

    Making hamburger menu open when click and closed when I click on nav in menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
+const navElm = document.querySelector('nav');
+const openMenu = () => {
+  navElm.classList.toggle('nav-closed');
+};
+const hamburgerMenu = document.querySelector('#nav-btn');
+hamburgerMenu.addEventListener('click', openMenu);
 
-console.log('funguju!');
+const listOfLinks = document.querySelectorAll('a');
+
+const closedMenu = () => navElm.classList.add('nav-closed');
+listOfLinks.forEach((item) => {
+  item.addEventListener('click', closedMenu);
+});
